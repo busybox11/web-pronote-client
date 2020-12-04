51b063146b96349c28df93c30e8cee3a7eb5bf6c
busybox11 <planetgeek11@gmail.com>
2020-12-04 23:08:08 +0100

    Bringing back the true colors of the mockup (kind of)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+  	borderRadius: {
+  		'none': '0',
+		'sm': '0.125rem',
+		DEFAULT: '1rem',
+		'md': '0.375rem',
+		'lg': '0.5rem',
+		'full': '9999px',
+		'large': '1.5rem'
+  	},
     extend: {},
   },
   variants: {
